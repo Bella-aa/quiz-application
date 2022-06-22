1bccf896b39688e0fdae4d899ed9b0ba3312c187
Bella-aa <Nyashamarara28@gmail.com>
2022-06-22 22:38:02 +0000

    questions added to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+const quizData = [
+
+    {
+    question: "What sport has a toe pick",
+    a: "Basketball",
+    b: "Hockey",
+    c: "Figure Skating",
+    d: "Golf",
+    correct: "c",
+
+    },
+
+    {
+        question: "In what part of the human body is the heart found? ",
+        a: "Foot ",
+        b: "Chest",
+        c: "Head",
+        d: "Arm",
+        correct: "b",
+    
+        },
+
+        {
+            question: "Which Minecraft mob explodes?",
+            a: "Enderman",
+            b: "Zombie",
+            c: " Bat",
+            d: "Creeper",
+            correct: "d",
+        
+            },
+
+            {
+                question: "Which type of creature is a crab? ",
+                a: "Crustacean ",
+                b: " Fish",
+                c: "Reptile ",
+                d: "Insect ",
+                correct: "a",
+            
+                },
+
+                {
+                    question: "Which of these fruits is shaped like a lemon?",
+                    a: " Tangerine",
+                    b: "Plum ",
+                    c: "Lime ",
+                    d: " Apple",
+                    correct: "c",
+                
+                    },
+
+                    {
+                        question: "What is the hottest colour of fire?",
+                        a: "Orange",
+                        b: "Yellow",
+                        c: "Purple",
+                        d: "Blue",
+                        correct: "d",
+                    
+                        },
+            
+        
+    
+
+
+    
+
+
+
+
+
+
+]
