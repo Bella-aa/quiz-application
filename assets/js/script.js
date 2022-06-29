6524a43b322db8981f4413292ebeb41f13b5f76b
Bella-aa <Nyashamarara28@gmail.com>
2022-06-29 12:16:36 +0000

    fixed errors on line 124

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,10 +120,11 @@ submitBtn.addEventListener('click' , () =>{
             quiz.innerHTML =
             `<h2>
             You answered ${score}/${quizData.length} questions correctly
-            </h2>`
+            </h2>
+
+            <button onclick="location.reload()">Reload</button>`
 
         }
 
-        ` <button onclick="location.reload()">Reload</button>`
     }
 })
